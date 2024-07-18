93b2a56e3e17c9e69a81b89fdab9519a319190f2
José Ruan <143905670+joseeruan@users.noreply.github.com>
2024-07-18 22:20:23 +0000

    first commit

---
+++ draft.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sqlite3
+
+data = pd.DataFrame({
+    'ID': [1, 2, 3, 4],
+    'Name': ['Alice', 'Bob', 'Charlie', 'David']
+})
+
+conn = sqlite3.connect('mydatabase.db')
+
+
+data.to_sql(
+    'client', conn,
+    if_exists='replace',
+    index=False
+)
+
+conn.close()
+
+conn = sqlite3.connect('mydatabase.db')
+
+query = """ 
+    SELECT Name 
+    FROM client
+    WHERE name IN ('Alice', 'David')
+"""
+
+pd.read_sql_query(query, conn)
